a90859fc83f3db9d72ca0349bdf577e850c5c7be
jen.rahman <jen.rahman@engineering.digital.dwp.gov.uk>
2025-10-22 10:34:39 +0100

    divider page
    GitOrigin-RevId: eb76901530b9d0836c09485134b7c784ba1f56f2

--- app/routes.js
+++ app/routes.js
@@ -801,3 +801,100 @@ router.post('/select-hcp10-manage', function (req, res) {
   }
 
 })
+
+
+
+
+/////
+//v11
+router.post('/manage-what11', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var manage = req.session.data['manage']
+
+  // Check whether the variable matches a condition
+  if (manage == "agents"){
+    // Send user to what language page
+    res.redirect('/v11/availability-management/agents/agent-profiles')
+  } else {
+    // Send user to next page
+    res.redirect('/v11/availability-management/availability/availability-view')
+  }
+
+})
+
+
+
+router.post('/status-check11', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var status = req.session.data['status']
+
+  // Check whether the variable matches a condition
+  if (status == "assessment-booked"){
+    // Send user to what language page
+    res.redirect('/v11/book-appt/update-status-assessment-booked')
+  } else {
+    // Send user to next page
+    res.redirect('/v11/book-appt/update-status-confirmation')
+  }
+
+})
+
+
+
+
+router.post('/select-hcp11', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var selectagent = req.session.data['select-agent']
+
+  // Check whether the variable matches a condition
+  if (selectagent == "choose-hcp"){
+    // Send user to what language page
+    res.redirect('/v11/book-appt/assign-hcp-while-booking')
+  } else {
+    // Send user to next page
+    res.redirect('/v11/book-appt/more-info')
+  }
+
+})
+
+
+
+router.post('/select-hcp11-manage', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var selectagent = req.session.data['select-agent']
+
+  // Check whether the variable matches a condition
+  if (selectagent == "choose-hcp"){
+    // Send user to what language page
+    res.redirect('/v11/has-manage-appt/assign-hcp-while-booking')
+  } else {
+    // Send user to next page
+    res.redirect('/v11/has-manage-appt/appt-details-change-hcp')
+  }
+
+})
+
+
+
+router.post('/cancel-cause', function (req, res) {
+
+  // Make a variable and give it the value from page
+  var apptreasoncancel = req.session.data['appt-reason-cancel']
+
+  // Check whether the variable matches a condition
+  if (apptreasoncancel == "Claimant"){
+    // Send user to what language page
+    res.redirect('/v11/book-appt/claimant-cancel-appointment')
+  } else {
+    // Send user to next page
+    res.redirect('/v11/book-appt/provider-cancel-appointment')
+  }
+
+})
+
+
+
